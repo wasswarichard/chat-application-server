54272c42387e533fbe36f5250421b81acda9652b
Wasswa Richard <wasswarichard55@gmail.com>
2021-02-25 18:57:19 +0300

    chat application set up

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ io.on('connection', (socket) => {
     socket.on('join', ({name, room}, callback) => {
         const {error, user} = addUser({id: socket.id, name, room});
         if (error) return callback(error);
+
+        // user admin messages
+        socket.emit('message', {text: `Hi ${user.name}, welcome to the ${user.room} room `});
+        socket.broadcast.to(user.room).emit('message', {text: `${user.name}, has joined`});
+
+        socket.join(user.room);
         getRoomMessages({user_room: user.room})
             .then(messages => {
-                console.log(messages);
+                io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room), messages});
             })
             .catch(error => {
                 console.log(error)
             });
-        socket.emit('message', {text: `${user.name}, welcome to the ${user.room} room `});
-        socket.broadcast.to(user.room).emit('message', {text: `${user.name}, has joined`})
-        socket.join(user.room);
-
-        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
         callback();
     });
 
@@ -33,7 +34,7 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
         postMessage({user_room: user.room, message: message, sent_by: user.name})
         io.to(user.room).emit('message', {user: user.name, text : message});
-        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+        // io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
         callback();
 
     });

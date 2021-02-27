58b78cffbed8edf4d376875c908e68ec5c022223
Wasswa Richard <wasswarichard55@gmail.com>
2021-02-27 10:23:55 +0300

    database config

--- config/database.js
+++ config/database.js
@@ -1,22 +1,26 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//     port: '5432',
+
+//     // user: 'postgres',
+//     // host: 'localhost',
+//     // database: 'application',
+//     // password: 'root',
+//
+//     // host: '34.93.81.97',
+//     // password: 'WtafTrl1',
+//     // database: 'postgres',
+//     // user: 'postgres',
+// });
+
+
 const pool = new Pool({
-    port: '5432',
-
-    host: 'ec2-54-211-77-238.compute-1.amazonaws.com',
-    database: 'dfbls2ch5b8sbn',
-    user: 'fzrhcdakikyvtp',
-    password: '0e57c54e1aec6aa1424ce06de7ac60463ae53522b6546a42013bb7b855d9c7cb',
-
-    // user: 'postgres',
-    // host: 'localhost',
-    // database: 'application',
-    // password: 'root',
-
-    // host: '34.93.81.97',
-    // password: 'WtafTrl1',
-    // database: 'postgres',
-    // user: 'postgres',
+    // connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://fzrhcdakikyvtp:0e57c54e1aec6aa1424ce06de7ac60463ae53522b6546a42013bb7b855d9c7cb@ec2-54-211-77-238.compute-1.amazonaws.com:5432/dfbls2ch5b8sbn',
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;

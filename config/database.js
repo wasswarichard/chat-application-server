d2ef2047459e643131b56db560598dca9ca0733a
Wasswa Richard <wasswarichard55@gmail.com>
2021-02-27 10:04:13 +0300

    database config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,22 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
+    port: '5432',
+
+    host: 'ec2-54-211-77-238.compute-1.amazonaws.com',
+    database: 'dfbls2ch5b8sbn',
+    user: 'fzrhcdakikyvtp',
+    password: '0e57c54e1aec6aa1424ce06de7ac60463ae53522b6546a42013bb7b855d9c7cb',
+
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'application',
+    // password: 'root',
+
     // host: '34.93.81.97',
-    host: 'localhost',
     // password: 'WtafTrl1',
-    password: 'root',
     // database: 'postgres',
-    database: 'application',
-    port: '5432'
+    // user: 'postgres',
 });
 
 module.exports = pool;
